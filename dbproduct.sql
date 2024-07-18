ea2cb1d012f7a232f29e62ddd894acafc429a59d
Stargazer <bezaleelbagoes@gmail.com>
2024-07-18 21:25:50 +0700

    update SQL Source file dbprouct

--- dbproduct.sql
+++ dbproduct.sql
@@ -1,3 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 18, 2024 at 04:23 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbproduct`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
 
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
@@ -7,6 +35,32 @@ CREATE TABLE `orders` (
   `date` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `customer_name`, `customer_address`, `total`, `date`) VALUES
+(1, 'Bezaleel', 'Dian Asri', 1480000.00, '2024-07-15'),
+(2, 'Miller', 'Washington D.C', 1230000.00, '2024-07-15'),
+(3, 'James', 'South Dakota', 1610000.00, '2024-07-15'),
+(4, 'Amiya', 'Rhodes Island', 830000.00, '2024-07-15'),
+(5, 'Kal\'tsit', 'Rhodes Island', 2400000.00, '2024-07-16'),
+(6, 'W', 'Rhodes Island', 1950000.00, '2024-07-15'),
+(7, 'Savage', 'Rhodes Island', 1330000.00, '2024-07-16'),
+(8, 'Mostima', 'Penguin Logistics', 2510000.00, '2024-07-16'),
+(9, 'Blaze', 'Rhodes Island', 500000.00, '2024-07-16'),
+(10, 'Firewatch', 'SAMI (NOW Rhodes Island)', 800000.00, '2024-07-16'),
+(11, 'Skadi', 'AEGIR', 900000.00, '2024-07-16'),
+(12, 'Specter', 'AEGIR', 500000.00, '2024-07-17'),
+(13, 'Miller', 'Aha', 430000.00, '2024-07-17'),
+(14, 'Frostleaf', 'Rhodes Island', 380000.00, '2024-07-17'),
+(15, 'Ptilopsis', 'Rhine Lab', 380000.00, '2024-07-18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
 
 CREATE TABLE `order_items` (
   `id` int(11) NOT NULL,
@@ -16,6 +70,62 @@ CREATE TABLE `order_items` (
   `total` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `quantity`, `total`) VALUES
+(1, 1, 5, 2, 1000000.00),
+(2, 1, 3, 1, 450000.00),
+(3, 1, 2, 1, 30000.00),
+(4, 2, 2, 1, 30000.00),
+(5, 2, 5, 1, 500000.00),
+(6, 2, 10, 1, 320000.00),
+(7, 2, 11, 1, 380000.00),
+(8, 3, 11, 1, 380000.00),
+(9, 3, 1, 1, 350000.00),
+(10, 3, 2, 1, 30000.00),
+(11, 3, 5, 1, 500000.00),
+(12, 3, 1, 1, 350000.00),
+(13, 4, 1, 1, 350000.00),
+(14, 4, 2, 1, 30000.00),
+(15, 4, 3, 1, 450000.00),
+(16, 5, 5, 1, 500000.00),
+(17, 5, 6, 2, 1200000.00),
+(18, 5, 8, 1, 350000.00),
+(20, 5, 8, 1, 350000.00),
+(21, 7, 8, 1, 350000.00),
+(22, 7, 5, 1, 500000.00),
+(23, 6, 5, 1, 500000.00),
+(24, 6, 5, 1, 500000.00),
+(25, 6, 5, 1, 500000.00),
+(26, 6, 3, 1, 450000.00),
+(27, 7, 3, 1, 450000.00),
+(28, 7, 2, 1, 30000.00),
+(29, 8, 2, 1, 30000.00),
+(30, 8, 1, 6, 2100000.00),
+(31, 8, 1, 1, 350000.00),
+(32, 8, 2, 1, 30000.00),
+(33, 9, 5, 1, 500000.00),
+(34, 10, 4, 2, 800000.00),
+(35, 10, 5, 1, 500000.00),
+(52, 10, 4, 1, 400000.00),
+(53, 10, 4, 1, 400000.00),
+(54, 11, 4, 1, 400000.00),
+(55, 11, 5, 1, 500000.00),
+(56, 13, 4, 1, 400000.00),
+(57, 13, 2, 1, 30000.00),
+(58, 12, 5, 1, 500000.00),
+(59, 14, 1, 1, 350000.00),
+(60, 14, 2, 1, 30000.00),
+(61, 15, 1, 1, 350000.00),
+(62, 15, 2, 1, 30000.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_tbl`
+--
 
 CREATE TABLE `product_tbl` (
   `id` int(11) NOT NULL,
@@ -24,6 +134,9 @@ CREATE TABLE `product_tbl` (
   `price` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `product_tbl`
+--
 
 INSERT INTO `product_tbl` (`id`, `name`, `type`, `price`) VALUES
 (1, 'Head First Java', 'Book', 350000.00),
@@ -77,33 +190,92 @@ INSERT INTO `product_tbl` (`id`, `name`, `type`, `price`) VALUES
 (49, 'Pattern Recognition and Machine Learning', 'Book', 520000.00),
 (50, 'Applied Predictive Modeling', 'Book', 510000.00);
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `user_tbl`
+--
+
+CREATE TABLE `user_tbl` (
+  `ID` int(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `code_user` varchar(255) NOT NULL,
+  `role` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `user_tbl`
+--
+
+INSERT INTO `user_tbl` (`ID`, `username`, `password`, `code_user`, `role`) VALUES
+(1, 'Admin', 'Admin123', '4dM1N123', 'ADMIN'),
+(2, 'User', 'User123', 'Us3R123', 'USER');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `orders`
+--
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`);
 
-
+--
+-- Indexes for table `order_items`
+--
 ALTER TABLE `order_items`
   ADD PRIMARY KEY (`id`),
   ADD KEY `order_id` (`order_id`),
   ADD KEY `product_id` (`product_id`);
 
-
+--
+-- Indexes for table `product_tbl`
+--
 ALTER TABLE `product_tbl`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `user_tbl`
+--
+ALTER TABLE `user_tbl`
+  ADD PRIMARY KEY (`ID`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
-
+--
+-- AUTO_INCREMENT for table `order_items`
+--
 ALTER TABLE `order_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
-
+--
+-- AUTO_INCREMENT for table `product_tbl`
+--
 ALTER TABLE `product_tbl`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `order_items`
+--
 ALTER TABLE `order_items`
-  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
-  ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product_tbl` (`id`)
+  ADD CONSTRAINT `FKbioxgbv59vetrxe0ejfubep1w` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  ADD CONSTRAINT `FKs7yce00lm3w3srckmhg7q4i24` FOREIGN KEY (`product_id`) REFERENCES `product_tbl` (`id`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
